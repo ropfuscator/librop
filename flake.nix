71227c1ebeccf6cb435908c43ab3c0888dd3f962
Giulio De Pasquale <depasquale+git@giugl.io>
2021-11-26 18:18:51 +0000

    no time to change script, revert to use clang

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/4b675777";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs32 = pkgs.pkgsi686Linux;
+      in {
+        defaultPackage = pkgs32.clangStdenv.mkDerivation {
+          pname = "librop";
+          version = "1.0.0";
+          nativeBuildInputs = with pkgs32; [ cmake python3 ];
+          src = ./.;
+          postPatch = "patchShebangs .";
+        };
+      });
+}
