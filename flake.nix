515a3b6775cf4624027d35f95d62fd64feee274e
Giulio De Pasquale <depasquale+git@giugl.io>
2022-04-23 22:12:26 +0200

    Use cross-compilation, override zlib (upstream bug)

--- flake.nix
+++ flake.nix
@@ -1,18 +1,39 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/4b675777";
+    nixpkgs.url = "github:peperunas/nixpkgs/llvm-i686-cross-fix";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        pkgs32 = pkgs.pkgsi686Linux;
-      in rec {
-        defaultPackage = pkgs32.callPackage ./pkg.nix {
-          stdenv = pkgs32.clangStdenv;
+        localSystem = { inherit system; };
+        crossSystem = {
+          config = "i686-unknown-linux-gnu";
+          useLLVM = true;
         };
+        pkgs = import nixpkgs {
+          inherit localSystem crossSystem;
+          overlays = [
+            (self: super: {
+              zlib =
+                if super.stdenv.hostPlatform != super.stdenv.buildPlatform then
+                  super.zlib.overrideAttrs (_: {
+                    version = "1.2.11";
+                    src = builtins.fetchurl {
+                      url = "https://www.zlib.net/fossils/zlib-1.2.11.tar.gz";
+                      sha256 =
+                        "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1";
+                    };
+                  })
+                else
+                  super.zlib;
+            })
+          ];
+        };
+      in rec {
+        defaultPackage =
+          pkgs.callPackage ./pkg.nix { stdenv = pkgs.clangStdenv; };
         packages = flake-utils.lib.flattenTree { librop = defaultPackage; };
       });
 }
